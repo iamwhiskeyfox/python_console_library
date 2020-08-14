d8f025d799141dfaa0fd745c7e0367c4cf975370
Dastan Bekzhanov <dastanbekzhanov7@gmail.com>
2020-08-14 21:45:30 +0600

    Edit sqlite_adapter.py, main.py and and books.db

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from books_app import BooksApp
 from DB.adapter import BookDBAdapter
+from DB.sqlite_adapter import SqliteBooksDbAdapter
 if __name__ == '__main__':
-    adapter = BookDBAdapter()
+    adapter = SqliteBooksDbAdapter('books.db')
     app = BooksApp(adapter)
     app.start()
 
