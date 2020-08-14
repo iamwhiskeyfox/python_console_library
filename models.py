340faa1567557bcd1acc37373fb94acd72983c14
Dastan Bekzhanov <dastanbekzhanov7@gmail.com>
2020-08-14 19:40:45 +0600

    Add Book model in models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, name, year, author, genre):
+        self.id = 0
+        self.name = name
+        self.year = year
+        self.author = author
+        self.genre = genre
